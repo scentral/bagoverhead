e9210171fe6c7a4deff584671cfc806a614b959b
Scentral <60952407+scentral@users.noreply.github.com>
2023-07-22 20:05:30 -0500

    Delete bag directory

--- bag/source/interface/script.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    window.addEventListener('message', function(event) {
-        if (event.data.action == "open") {
-            $(".bag").fadeIn(150);
-        } else if (event.data.action == "close") {
-            $(".bag").fadeOut(150);
-        }
-    })
-})
--- bag/source/server.lua
+++
@@ -1,4 +0,0 @@
-RegisterNetEvent("bag:check")
-AddEventHandler("bag:check", function(player, hasBag)
-    TriggerClientEvent("bag:check:client", player, hasBag)
-end)
